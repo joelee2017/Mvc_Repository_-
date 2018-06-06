78d37743374bd0ca498b7e01d3608511420dbf7b
joe lee <as955133@gmail.com>
2018-06-06 16:35:06 +0800

    GenericRepository 建立

--- Mvc_Repository/Models/Repository/GenericRepository.cs
+++ Mvc_Repository/Models/Repository/GenericRepository.cs
@@ -1,7 +1,10 @@
 ﻿using Mvc_Repository.Models.Interface;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 
 namespace Mvc_Repository.Models.Repository
@@ -9,39 +12,108 @@ namespace Mvc_Repository.Models.Repository
     public class GenericRepository<TEntity> : IRepository<TEntity>
         where TEntity : class
     {
+        private TestDBEntities testDBEntities;
+
+        private DbContext _context
+        {
+            get;
+            set;
+        }
+
+        public GenericRepository()
+            : this(new TestDBEntities())
+        {
+
+        }
+
+        public GenericRepository(DbContext context)
+        {
+            if(context ==null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            this._context = context;
+        }
+
+        public GenericRepository(ObjectContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            this._context = new DbContext(context, true);
+        }
+
         public void Create(TEntity instance)
         {
-            throw new NotImplementedException();
+           if(instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                this._context.Set<TEntity>().Add(instance);
+                this.SaveChanges();
+            }
         }
 
         public void Update(TEntity instance)
         {
-            throw new NotImplementedException();
+            if(instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                this._context.Entry(instance).State = EntityState.Modified;
+                this.SaveChanges();
+            }
         }
 
         public void Delete(TEntity instance)
         {
-            throw new NotImplementedException();
-        }      
+            if(instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                this._context.Entry(instance).State = EntityState.Deleted;
+                this.SaveChanges();
+            }
+        }
 
-        public TEntity Get(int PrimaryID)
+        public TEntity Get(Expression<Func<TEntity, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return this._context.Set<TEntity>().FirstOrDefault(predicate);
         }
 
         public IQueryable<TEntity> GetAll()
         {
-            throw new NotImplementedException();
+            return this._context.Set<TEntity>().AsQueryable();
         }
 
         public void SaveChanges()
         {
-            throw new NotImplementedException();
-        }              
+            this._context.SaveChanges();
+        }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                if(this._context != null)
+                {
+                    this._context.Dispose();
+                    this._context = null;
+                }
+            }
         }
     }
 }
