70c8c195725c7b78abcb1dc291550314d87cca23
joe lee <as955133@gmail.com>
2018-06-06 12:13:54 +0800

    CategoryController  手工建置

---
+++ Mvc_Repository/Controllers/CategoryController.cs
@@ -0,0 +1,123 @@
+﻿using Mvc_Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Repository.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            using (TestDBEntities db = new TestDBEntities())
+            {
+                var query = db.Categories.OrderBy(x => x.CategoryID);
+                ViewData.Model = query.ToList();
+                return View();
+            }
+        }
+        //=====================================================================
+
+        public ActionResult Details(int? id)
+        {
+            if(!id.HasValue)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                using (TestDBEntities db = new TestDBEntities())
+                {
+                    var model = db.Categories.FirstOrDefault(x => x.CategoryID == id.Value);
+                    ViewData.Model = model;
+                    return View();
+                }
+            }
+        }
+        //======================================================================
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Categories category)
+        {
+            if(category != null && ModelState.IsValid)
+            {
+                using (TestDBEntities db = new TestDBEntities())
+                {
+                    db.Categories.Add(category);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View(category);
+            }
+        }
+        //======================================================================
+
+        [HttpPost]
+        public ActionResult Edit(Categories category)
+        {
+            if(category != null && ModelState.IsValid)
+            {
+                using (TestDBEntities db = new TestDBEntities())
+                {
+                    db.Entry(category).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return View(category);
+                }
+            }
+            else
+            {
+                return RedirectToAction("index");
+            }
+        }
+        //======================================================================
+
+        public  ActionResult Delete(int? id)
+        {
+            if(!id.HasValue)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                using (TestDBEntities db = new TestDBEntities())
+                {
+                    var model = db.Categories.FirstOrDefault(x => x.CategoryID == id.Value);
+                    ViewData.Model = model;
+                    return View();
+                }
+            }
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                using (TestDBEntities db = new TestDBEntities())
+                {
+                    var target = db.Categories.FirstOrDefault(x => x.CategoryID == id);
+                    db.Categories.Remove(target);
+                    db.SaveChanges();
+                }
+            }
+            catch(DataException)
+            {
+                return RedirectToAction("Delete", new { id = id });
+            }
+            return RedirectToAction("index");
+        }
+    }
+}
