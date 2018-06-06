c54348c6a11d8b8f8a2c635459e0474b05df3616
joe lee <as955133@gmail.com>
2018-06-06 14:13:57 +0800

    CategoryRepository.cs 建立

---
+++ Mvc_Repository/Models/Interface/ICategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_Repository.Models.Repository
+{
+    public interface ICategoryRepository
+    {
+        void Create(Categories instance);
+
+        void Update(Categories instance);
+
+        void Delete(Categories instance);
+
+        Categories Get(int categoryID);
+
+        IQueryable<Categories> GetAll();
+
+        void SaveChanges();
+    }
+}
---
+++ Mvc_Repository/Models/Repository/CategoryRepository.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_Repository.Models.Repository
+{
+    public class CategoryRepository : ICategoryRepository, IDisposable
+    {
+        protected TestDBEntities db
+        {
+            get;
+            private set;
+        }
+
+        public CategoryRepository()
+        {
+            this.db = new TestDBEntities();
+        }
+
+        public void Create(Categories instance)
+        {
+           if(instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                db.Categories.Add(instance);
+                this.SaveChanges();
+            }
+        }
+
+        public void Update(Categories instance)
+        {
+            if(instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                db.Entry(instance).State = EntityState.Modified;
+                this.SaveChanges();
+            }
+        }
+
+        public void Delete(Categories instance)
+        {
+            if(instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                db.Entry(instance).State = EntityState.Deleted;
+                this.SaveChanges();
+            }
+        }
+      
+        public Categories Get(int categoryID)
+        {
+            return db.Categories.FirstOrDefault(x => x.CategoryID == categoryID);
+        }
+
+        public IQueryable<Categories> GetAll()
+        {
+            return db.Categories.OrderBy(x => x.CategoryID);
+        }
+
+        public void SaveChanges()
+        {
+            this.db.SaveChanges();
+        }    
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+           if(disposing)
+            {
+                if(this.db != null)
+                {
+                    this.db.Dispose();
+                    this.db = null;
+                }
+            }
+        }
+    }
+}
