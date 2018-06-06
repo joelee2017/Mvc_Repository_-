ba1847ecad0b8d770bbaa93e58830739b6e1d3b4
joe lee <as955133@gmail.com>
2018-06-06 15:07:28 +0800

    IRepository  介面建立

---
+++ Mvc_Repository/Models/Interface/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_Repository.Models.Interface
+{
+    public interface IRepository<TEntity> :IDisposable
+        where TEntity :class
+    {
+        void Create(TEntity instance);
+
+        void Update(TEntity instance);
+
+        void Delete(TEntity instance);
+
+        TEntity Get(int PrimaryID);
+
+        IQueryable<TEntity> GetAll();
+
+        void SaveChanges();
+    }
+}
